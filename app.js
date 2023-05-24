211f37af3b88d9476e05be7fa5ac026a8d90098a
Muhammd Faizan Alam <121306012+faizan-smit@users.noreply.github.com>
2023-05-24 19:42:23 +0500

    form submit

---
+++ app.js
@@ -0,0 +1,78 @@
+
+
+let foo = ()=>{
+
+
+    let newsApi = "79f42ab294404ac4a46d06a09336be0b";
+
+    let search = document.getElementById('search').value
+    axios.get(`https://newsapi.org/v2/everything?q=${search}&from=2023-04-24&sortBy=publishedAt&apiKey=${newsApi}`)
+
+    .then((news)=>{
+
+
+        // news.data.articles.array.foreach(element => {
+
+
+        //     let div1 = document.createElement('div');
+        //     let span1 = document.createElement('span');
+        //     let para = document.createElement('span');
+        //     let maindiv = document.getElementById('maindiv');
+
+        //     let heading = document.createTextNode(element.title)
+        //     let article = document.createTextNode(element.description)
+        //     span1.appendChild(heading)
+        //     para.appendChild(article)
+        //     div1.appendChild(span1, para)
+        //     maindiv.appendChild(div1)
+
+            
+        // });
+
+        news.data.articles.forEach(element => {
+
+                let div1 = document.createElement('div');
+            let span1 = document.createElement('span');
+            let para = document.createElement('span');
+            let maindiv = document.getElementById('maindiv');
+
+            let heading = document.createTextNode(element.title)
+            let article = document.createTextNode(element.content)
+            let newHeading = "<q>" + element.title + "</q>"
+            let newArticle = "<blockquote>" + element.content + "<blockquote>"
+            // span1.appendChild(newHeading)
+            span1.innerHTML = newHeading
+            // para.appendChild(article)
+            para.innerHTML = newArticle
+            span1.className = 'heading';
+            para.className = 'content';
+            div1.appendChild(span1)
+            div1.appendChild(para)
+            maindiv.appendChild(div1)
+            console.log(newHeading)
+
+            
+            
+        });
+
+
+        console.log(news.data.articles)
+    console.log(newHeading)
+
+        
+        
+       
+       
+       // console.log(res.data.main.temp)
+        // console.log(res.data.main.pressure)
+        // console.log(res.data.main.temp_max)
+        // console.log(res.data.main.temp_min)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+
+    // console.log(newHeading)
+
+
+}
